800439da469465c5ef15df166177ecb6104806af
RLC <rlcarmody@gmail.com>
2019-03-09 12:05:36 -0800

    Mapping and location services

--- places.js
+++ places.js
@@ -1,5 +1,5 @@
 const getPlaces = (location, section = 'topPicks') => {
-    const fourSquareURL = 'https://api.foursquare.com/v2/venues/explore?v=20190307&client_id=VSKZRJ3CZT5J0WQK2AEAPSP254O4SJAUEE1WVASQSY4NCSBR&client_secret=OTQKUUM5KKMFALEKGS3RYRZUD1OGRIC0GV4YCDTCI3KWOSLR&ll=';
+    const fourSquareURL = 'https://api.foursquare.com/v2/venues/explore?limit=50&v=20190307&client_id=VSKZRJ3CZT5J0WQK2AEAPSP254O4SJAUEE1WVASQSY4NCSBR&client_secret=OTQKUUM5KKMFALEKGS3RYRZUD1OGRIC0GV4YCDTCI3KWOSLR&ll=';
     return $.ajax({
         url: fourSquareURL + location + '&section=' + section,
         method: 'GET'
@@ -14,18 +14,34 @@ const geoCode = async query => {
     })
 }
 
-$('button').on('click', function () {
+const search = query => {
     $('#stuff').empty();
-    geoCode($('input').val())
+    geoCode(query)
         .then(response => {
-            let ll = response.resourceSets[0].resources[0].geocodePoints[0].coordinates.toString();
-            getPlaces(ll).then(response => {
-                console.log(response)
+            try {
+            let ll = response.resourceSets[0].resources[0].geocodePoints[0].coordinates.toString().replace(',', '%2C');
+            getPlaces(ll).then(response => {//replace with call to updated places UI
                 response.response.groups[0].items.forEach(e => {
-                    $('#stuff').append(`<p>${e.venue.name}</p>`)
+                    $('#stuff').append(`<div><a href="https://www.google.com/maps/search/?api=1&query=${e.venue.location.lat + '%2C' + e.venue.location.lng}" target="_blank">${e.venue.name}</a><img src="${e.venue.categories[0].icon.prefix + '64' + e.venue.categories[0].icon.suffix}" alt="Photo of ${e.venue.name}"/></div>`)
                 })
             })
+        } catch {
+            $('#stuff').text('Sorry, no results found');
+        }
             //.then do something
             //get weather call too
         });
+    }
+
+$('#search').on('click', function () {
+    let location = $('input').val(); 
+    search(location) 
+})
+$('#locate').on('click', function () {
+    let location = '';
+    window.navigator.geolocation.getCurrentPosition(response => {
+        location = response.coords.latitude + ',' + response.coords.longitude;
+        search(location);
+    });
+    
 })
