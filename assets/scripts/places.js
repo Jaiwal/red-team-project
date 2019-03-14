ef8db7f06acff3e00b0d0a80222a0cdd47a7a2ed
RLC <rlcarmody@gmail.com>
2019-03-13 18:20:54 -0700

    Polishing UI

--- assets/scripts/places.js
+++ assets/scripts/places.js
@@ -22,34 +22,6 @@ $(document).ready(function () {
             credentials: 'Ao5GBZzTBhN_P14V5wnkRm5gX7oxMjW47tpMlDtE_qVc16qRn7ElPOlM3IxfzPQM',
             center: mapTarget,
             zoom: 17,
-            // mapTypeId: Microsoft.Maps.MapTypeId.canvasDark,
-            // customMapStyle: {
-            //     elements: {
-            //         street: {
-            //             fillColor: '#8c7b79',
-            //         },
-            //         water: {
-            //             fillColor: '#4D76B2'
-            //         },
-            //         highway: {
-            //             fillColor: '#8FA9CC'
-            //         },
-            //         road: {
-            //             fillColor: '#403F4C',
-            //             strokeColor: '#ccc'
-            //         },
-            //         vegetation: {
-            //             fillColor: '#6C7F2A'
-            //         },
-            //         mapElement: {
-            //             labelColor: '#fff',
-            //             labelOutlineColor: '#666'
-            //         }
-            //     },
-            //     settings: {
-            //         landColor: '#51637E'
-            //     }
-            // }
         });
         infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
             visible: false,
@@ -72,14 +44,15 @@ $(document).ready(function () {
             method: 'GET'
         }).then(response => {
             let photoURL = response.response.venue.bestPhoto.prefix + '300x300' + response.response.venue.bestPhoto.suffix;
-            let timeframes = response.response.venue.popular.timeframes;
             let hours = '';
-            if (!timeframes) {
+            try {
                 hours = '<table>'
                 response.response.venue.popular.timeframes.forEach(e => {
                     hours += `<tr><td>${e.days}</td><td>${e.open[0].renderedTime}</td><tr>`
                 });
                 hours += '</table>';
+            } catch (e) {
+                hours = 'Hours not available'
             }
 
             let venueLocation = new Microsoft.Maps.Location(response.response.venue.location.lat, response.response.venue.location.lng);
@@ -107,7 +80,7 @@ $(document).ready(function () {
         $('#results').remove();
         $('body').append('<div class="row" id="weather">');
         $('body').append('<div class="row no-gutters" id="results">');
-        $('#results').append('<div class="col-4"><ul class="list-group"></div><div class="col-8"><div id="map"></div></div>')
+        $('#results').append('<div class="col-4" id="leftcol"><ul class="list-group"></div><div class="col-8"><div id="map"></div></div>')
     }
 
     const search = (query, section) => {
@@ -117,17 +90,16 @@ $(document).ready(function () {
                 let llarr = response.resourceSets[0].resources[0].geocodePoints[0].coordinates;
                 let ll = llarr.toString().replace(',', '%2C');
                 getMap(llarr);
-                getPlaces(ll, section).then(response => {//replace with call to updated places UI
+                getPlaces(ll, section).then(response => {
                     let layer = new Microsoft.Maps.Layer();
                     let pins = [];
                     let pinsbg = [];
                     response.response.groups[0].items.forEach(e => {
-                        $('.list-group').append(`<li class="list-group-item list-group-item-action" data-id="${e.venue.id}" data-name="${e.venue.name}">${e.venue.name} <span class="info""></span></li>`)
+                        $('.list-group').append(`<li class="list-group-item list-group-item-action" data-id="${e.venue.id}" data-name="${e.venue.name}">${e.venue.name}</li>`)
                         let place = new Microsoft.Maps.Location(e.venue.location.lat, e.venue.location.lng);
                         let pbg = new Microsoft.Maps.Pushpin(place, {
                             icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAMUlEQVRYR+3QQREAAAjDMOZt/i2BDD6pgl7SduexGCBAgAABAgQIECBAgAABAgS+BQ6OUzmBVuXhXAAAAABJRU5ErkJggg=='
                         });
-                        //map.entities.push(pbg);
                         pinsbg.push(pbg);
                         let pin = new Microsoft.Maps.Pushpin(place, {
                             title: e.venue.name,
@@ -141,7 +113,6 @@ $(document).ready(function () {
                         Microsoft.Maps.Events.addHandler(pin, 'click', function (e) {
                             ppClicked(e.target.metadata.venueID, e.target.metadata.title)
                         });
-                        //map.entities.push(pin);
                         pins.push(pin);
                     })
                     map.entities.push(pinsbg);
@@ -149,7 +120,7 @@ $(document).ready(function () {
                     map.layers.insert(layer);
                 })
             } catch (e) {
-                $('#stuff').text('Sorry, no results found');
+                $('#leftcol').text('Sorry, no results found');
             }
         })
     }
@@ -167,11 +138,35 @@ $(document).ready(function () {
         let location = '';
         window.navigator.geolocation.getCurrentPosition(response => {
             location = response.coords.latitude + ',' + response.coords.longitude;
-
-            search(location);
+            if (!$('input:radio[name="categories"]:checked').val()) {
+                search(location)
+            } else {
+                search(location, $('input:radio[name="categories"]:checked').val())
+            }
         });
     })
     $('body').on('click', 'li', function () {
         ppClicked($(this).attr('data-id'), $(this).attr('data-name'));
     });
+
+    $('input:radio[name="categories"]').on('click', function () {
+        let activeSel = $('input[name="categories"]:checked').val();
+        switch (activeSel) {
+            case 'food':
+                $('#nav').css('background-image', 'url(assets/images/foodbg.jpg)');
+                break;
+            case 'drinks':
+                $('#nav').css('background-image', 'url(assets/images/drinksbg.jpeg)');
+                break;
+            case 'arts':
+                $('#nav').css('background-image', 'url(assets/images/artsbg.jpeg)');
+                break;
+            case 'outdoors':
+                $('#nav').css('background-image', 'url(assets/images/park.jpeg)');
+                break;
+            case undefined:
+                $('#nav').css('background-image', 'url(assets/images/beachfront.jpeg)');
+        }
+    })
 })
+
